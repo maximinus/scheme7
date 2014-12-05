30ea28d138e9935cdbedd790c2d99729c932773c
maximinus <maximinus@gmail.com>
2014-12-05 17:38:19 +0800

    Clean-up for end of day

--- js/scheme7.js
+++ js/scheme7.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var game = new Phaser.Game(400, 600, Phaser.CANVAS, 'Scheme7', {preload:preload, create:create, update:update});
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Scheme7', {preload:preload, create:create, update:update});
 
 function Ship() {
 	this.xpos = 0;
@@ -10,6 +10,10 @@ function Ship() {
 	this.polygons = new Array();
 	
 	this.addPolygon = function(points) {
+		for(var i in points) {
+			points[i]['x'] += this.xpos;
+			points[i]['y'] += this.ypos;
+		}
 		var poly = new Phaser.Polygon([]);
 		poly.setTo(points);
 		this.polygons.push(poly);
@@ -29,11 +33,13 @@ function preload() {
 
 function create() {
 	var player = new Ship();
-	player.addPolygon([{x:0, y:0}, {x:100, y:240}, {x:0, y:240}]);
 	player.ypos = 50;
-	player.xpos = 50;
+	player.xpos = 150;
+	player.addPolygon([{x:0, y:0}, {x:-100, y:240}, {x:0, y:200}]);
+	player.addPolygon([{x:0, y:0}, {x:0, y:200}, {x:100, y:240}]);
 
 	var gfx = game.add.graphics(0, 0);
+	console.log(gfx);
 	player.render(gfx);
 };
 
