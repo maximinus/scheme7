d3ccf469337993a87556a8a0674d40541562db2d
maximinus <maximinus@gmail.com>
2014-12-06 15:42:34 +0800

    Got simple physics working

--- js/scheme7.js
+++ js/scheme7.js
@@ -4,11 +4,14 @@ var WIDTH = 800;
 var HEIGHT = 600;
 
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS, 'Scheme7', {preload:preload, create:create, update:update});
+var cursors;
+var player;
+var flag;
 
 function setupPhysics() {
 	game.physics.startSystem(Phaser.Physics.P2JS);
-	game.physics.p2.gravity.y = 10;
-    game.physics.p2.restitution = 0.8;
+	game.physics.p2.gravity.y = 24;
+    game.physics.p2.restitution = 0.2;
 };
 
 function preload() {
@@ -19,19 +22,35 @@ function preload() {
 function create() {
 	setupPhysics();
 
-	var player = game.add.sprite(350, 150, 'ship');
+	player = game.add.sprite(350, 150, 'ship');
 	// true is for the visual debugger
-	game.physics.p2.enable(player, false);
+	game.physics.p2.enable(player, true);
 	player.body.clearShapes();
-	player.body.addPolygon({}, [[0,0], [-25,60], [25,60],[0,0]]);
+	player.body.addPolygon({}, [[0,0], [-25,60], [25,60], [0,0]]);
 
 
 	var ground = game.add.sprite(0, 550, 'ground');
-	game.physics.p2.enable(ground, true);
+	game.physics.p2.enable(ground, false);
 	ground.body.clearShapes();
 	ground.body.addPolygon({}, [[0,0], [WIDTH,0], [WIDTH,5], [0,5], [0,0]]);
+	ground.body.static = true;
+	
+	game.camera.follow(player);
+	
+	cursors = game.input.keyboard.createCursorKeys();
 };
 
 function update() {
+	if(cursors.left.isDown) {
+		player.body.rotateLeft(40);
+		flag = true; }
+	else if(cursors.right.isDown) {
+		player.body.rotateRight(40);
+		flag = true; }
+	else if(flag == true) {
+		player.body.setZeroRotation();
+		flag = false; }
+	if(cursors.up.isDown) {
+		player.body.thrust(80); };
 };
 
