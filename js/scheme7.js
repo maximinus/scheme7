b64461faa3d9fa14c1227cd01f5ab088d11be87d
maximinus <maximinus@gmail.com>
2014-12-06 17:55:25 +0800

    Basic level built of fabricated sprites

--- js/scheme7.js
+++ js/scheme7.js
@@ -8,35 +8,55 @@ var cursors;
 var player;
 var flag;
 
+function generateSpriteImage(width, height) {
+	var image = game.add.bitmapData(width, height);
+	// clear to black and draw lines all over
+	image.ctx.fillStyle = '#000000';
+	image.ctx.fillRect(0, 0, width, height);
+	// top, bottom, left, right lines
+	image.ctx.strokeStyle = '#ff00ff';
+	image.ctx.lineWidth = 1;
+	image.ctx.strokeRect(0, 0, width, height);
+	// now we can make the sprite
+	return(image);
+};
+
 function setupPhysics() {
 	game.physics.startSystem(Phaser.Physics.P2JS);
 	game.physics.p2.gravity.y = 24;
     game.physics.p2.restitution = 0.2;
 };
 
-function preload() {
-	game.load.image('ship', 'gfx/ship.png');
-	game.load.image('ground', 'gfx/ground.png');
-};
-
-function create() {
-	setupPhysics();
-
+function buildPlayer() {
 	player = game.add.sprite(350, 150, 'ship');
 	// true is for the visual debugger
 	game.physics.p2.enable(player, true);
 	player.body.clearShapes();
-	player.body.addPolygon({}, [[0,0], [-25,60], [25,60], [0,0]]);
+	player.body.addPolygon({}, [[0,0], [-25,60], [25,60]]);
+};
 
+function buildLevel() {
+	// the array is xpos, ypos, height, width
+	var boxes = [[20, 20, 760, 10], [20, 30, 10, 520], [20, 550, 760, 10], [770, 30, 10, 520]];
+	for(var i in boxes) {
+		var image = generateSpriteImage(boxes[i][2], boxes[i][3]);
+		var sprite = game.add.sprite(boxes[i][0], boxes[i][1], image);
+		game.physics.p2.enable(sprite, false);
+		sprite.body.clearShapes();
+		sprite.body.addPolygon({}, [[0, 0],	[boxes[i][2], 0], [boxes[i][2], boxes[i][3]], [0, boxes[i][3]]]);
+		sprite.body.static = true;
+	}
+};
 
-	var ground = game.add.sprite(0, 550, 'ground');
-	game.physics.p2.enable(ground, false);
-	ground.body.clearShapes();
-	ground.body.addPolygon({}, [[0,0], [WIDTH,0], [WIDTH,5], [0,5], [0,0]]);
-	ground.body.static = true;
-	
-	game.camera.follow(player);
-	
+function preload() {
+	game.load.image('ship', 'gfx/ship.png');
+};
+
+function create() {
+	setupPhysics();
+	buildPlayer();
+	buildLevel();
+	//game.camera.follow(player);
 	cursors = game.input.keyboard.createCursorKeys();
 };
 
