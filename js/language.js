f243a267bcc5eb078324fdde4d7c1943dd7b929c
maximinus <maximinus@gmail.com>
2014-12-14 15:57:13 +0800

    Started parsing code

--- js/language.js
+++ js/language.js
@@ -1,78 +1,160 @@
 "use strict";
 
+// enums for parser types
+var PARSE_TYPES = {'OPEN':0, 'CLOSE':1, 'IDENTIFIER':2, 'NUMBER':3, 'STRING':4};
+
 // start with an environment
 var Env = {};
-var token_error = false;
 
-function setTokenError(text) {
-	token_error = true;
-};
+// code for the tokeniser
+// this turns the raw text into an array of tokens
+function Tokeniser() {
+	this.token_error = false;
+	this.error_message = '';
 
-function spaceParens(text) {
-	// put spaces around the parens:
-	text = text.replace(/\(/g, ' ( ');
-	text = text.replace(/\)/g, ' ) ');
-	return(text);
-};
+	this.setTokenError = function(text) {
+		this.token_error = true;
+		this.error_message = text;
+	};
 
-function removeEmptyStrings(tokens) {
-	// remove null strings at start and end of the list
-	return(tokens.filter(function(value) { return(value != ''); }));
-};
+	this.spaceParens = function(text) {
+		// put spaces around the parens:
+		text = text.replace(/\(/g, ' ( ');
+		text = text.replace(/\)/g, ' ) ');
+		return(text);
+	};
 
-function joinStrings(tokens) {
-	// traverse the array looking for one that starts with a "
-	// token is always a string of length > 0
-	var new_tokens = [];
-	for(var i=0; i<tokens.length; i++) {
-		// does it start with "?
-		if(textIsNotFullyQuoted(tokens[i])) {
-			var index = readUntilQuote(tokens.slice(i + 1));
-			// now we know how long the quote is, so create it and store
-			console.log(tokens.slice(i, i+index));
-			new_tokens.push(tokens.slice(i, i+index).join());
-			// 1 is added to i next iteration
-			i += index;
+	this.removeEmptyStrings = function(tokens) {
+		// remove null strings at start and end of the list
+		return(tokens.filter(function(value) { return(value != ''); }));
+	};
+
+	this.joinStrings = function(tokens) {
+		// traverse the array looking for one that starts with a "
+		// token is always a string of length > 0
+		var new_tokens = [];
+		for(var i=0; i<tokens.length; i++) {
+			// does it start with "?
+			if(this.textIsNotFullyQuoted(tokens[i])) {
+				var index = this.readUntilQuote(tokens.slice(i + 1));
+				// now we know how long the quote is, so create it and store
+				new_tokens.push(tokens.slice(i, i+index).join());
+				// 1 is added to i next iteration
+				i += index;
+			}
+			else {
+				new_tokens.push(tokens[i]); }
 		}
-		else {
-			new_tokens.push(tokens[i]);
+		return(new_tokens);
+	};
+
+	this.readUntilQuote = function(tokens) {
+		for(var i=0; i<tokens.length;i++) {
+			if(this.endIsQuote(tokens[i])) {
+				// +1 because we need the offset (and arrat is zero-indexed)
+				return(i + 1); }
 		}
-	}
-	return(new_tokens);
+		// there is an error
+		this.setTokenError('String does not end');
+		return(-1);
+	};
+
+	this.textIsNotFullyQuoted = function(text) {
+		// test further if start is a quote
+		if(text[0] != '"') {
+			return(false); }
+		return(!this.endIsQuote(text));
+	};
+
+	this.endIsQuote = function(text) {
+		// only length 1, easy:
+		if(text.length == 1) {
+			return(text[0] == '"'); }
+		// otherwise, have to check for \ before:
+		return((text[text.length-1] == '"') && (text[text.length-2] != '\\'));
+	};
+
+	this.tokenise = function(text) {
+		text = this.spaceParens(text);
+		var tokens = text.split(/\s+/);
+		tokens = this.removeEmptyStrings(tokens);
+		tokens = this.joinStrings(tokens);
+		return({'tokens':tokens, error:this.token_error, error_message:this.error_message});
+	};
 };
 
-function readUntilQuote(tokens) {
-	var i;
-	for(i=0; i<tokens.length;i++) {
-		if(endIsQuote(tokens[i])) {
-			// +1 because we need the offset (and arrat is zero-indexed)
-			return(i + 1); }
-	}
-	// there is an error
-	setTokenError('String does not end');
-	return(-1);
+// now the parser. We have to define an 'item'. It could be a number, or a string, or an identifier
+function ParseItem(type, value) {
+	this.type = type;
+	this.value = value || null;
 };
 
-function textIsNotFullyQuoted(text) {
-	// test further is start is not a quote
-	if(text[0] != '"') {
-		return(false); }
-	return(!endIsQuote(text));
-};
+function Parser() {
+	this.parse_error = false;
+	this.error_message = '';
 
-function endIsQuote(text) {
-	// only length 1, easy:
-	if(text.length == 1) {
-		return(text[0] == '"'); }
-	// otherwise, have to check for \ before:
-	return((text[text.length-1] == '"') && (text[text.length-2] != '\\'));
-};
+	this.setParseError = function(text) {
+		this.parse_error = true;
+		this.error_message = text;
+	};
 
-function tokenise(text) {
-	text = spaceParens(text);
-	var tokens = text.split(/\s+/);
-	tokens = removeEmptyStrings(tokens);
-	tokens = joinStrings(tokens);
-	return(tokens);
+	this.parensBalanced = function(tokens) {
+		// must be more than one token
+		if(tokens.length < 2) {
+			return(false); }
+		// opening and closing must be correct
+		if((tokens[0] != '(') || (tokens[tokens.length - 1] != ')')) {
+			return(false); }
+		var open = 0;
+		for(var i in tokens) {
+			if(tokens[i] == '(') {
+				open++; }
+			else if(tokens[i] == ')') {
+				open--; }
+			// sum of ) must be always less than (
+			if(open < 0) {
+				return(false); }
+		};
+		// equal number of ( and )
+		return(open == 0);
+	};
+
+	this.tokenIsString = function(token) {
+		if(token.length < 2) {
+			return(false); }
+		if((token[0] == '"') && (token[token.length - 1] == '"')) {
+			return(true); }
+		// some error
+		this.setParseError('Malformed string');
+		return(false);
+	};
+
+	this.stripQuotes = function(token) {
+		// from tokenIsString, we know at least an array of 2 items
+		return(token.slice(1, token.length - 1));
+	};
+
+	this.getParseItem = function(token) {
+		// given a token, what is it?
+		if(token == '(') {
+			return(ParseItem(PARSE_TYPES.OPEN)); }
+		if(token == ')') {
+			return(ParseItem(PARSE_TYPES.CLOSE)); }
+		// number? is ddd or dd.ddd
+		if((token.match(/^[0-9]+$/)) || (token.match(/^[0-9]+\.[0-9]+/))) {
+			return(ParseItem(PARSE_TYPES.NUMBER), Number(token)); }
+		if(this.tokenIsString(token)) {
+			return(ParseItem(PARSE_TYPES.STRING), this.stripQuotes(token)); }
+	};
+
+	// convert the tokens into values
+	this.parseTokens = function(token_list) {
+		// go along tokens converting
+		if(!parensBalanced) {
+			this.setParseError('Parens unbalanced');
+			return([]); }
+		var parse_results = [];
+		parse_type
+	};
 };
 
--- tests/tests.js
+++ tests/tests.js
@@ -1,47 +1,88 @@
 "use strict";
 
+var t = new Tokeniser();
+var p = new Parser();
+
+QUnit.test('Tokenise error test 1', function(assert) {
+	t.setTokenError('test');
+	assert.equal(t.token_error, true, 'Must show tokeniser error');
+});
+
+QUnit.test('Parser error test 1', function(assert) {
+	p.setParseError('test');
+	assert.equal(p.parse_error, true, 'Must show parser error');
+});
+
 QUnit.test('Parens test 1', function(assert) {
 	var test = '(+ 2 3)';
-	assert.equal(spaceParens(test), ' ( + 2 3 ) ', 'Parens must have spaces');
+	assert.equal(t.spaceParens(test), ' ( + 2 3 ) ', 'Parens must have spaces');
 });
 
 QUnit.test('Parens test 2', function(assert) {
 	var test = '(+ (+ 1) 2 3)';
-	assert.equal(spaceParens(test), ' ( +  ( + 1 )  2 3 ) ', 'Parens must have spaces');
+	assert.equal(t.spaceParens(test), ' ( +  ( + 1 )  2 3 ) ', 'Parens must have spaces');
 });
 
 QUnit.test('Remove empty strings 1', function(assert) {
 	var tokens = ['', '2', ''];
-	assert.equal(removeEmptyStrings(tokens).length, 1, 'Empty strings must be removed');
+	assert.equal(t.removeEmptyStrings(tokens).length, 1, 'Empty strings must be removed');
 });
 
 QUnit.test('Remove empty strings 2', function(assert) {
 	var tokens = [];
-	assert.equal(removeEmptyStrings(tokens).length, 0, 'Empty strings must be removed');
+	assert.equal(t.removeEmptyStrings(tokens).length, 0, 'Empty strings must be removed');
 });
 
 QUnit.test('Token length test 1', function(assert) {
 	var test = '(+ 2 3)';
-	assert.equal(tokenise(test).length, 5, 'Number of tokens must be consistent');
+	assert.equal(t.tokenise(test).tokens.length, 5, 'Number of tokens must be consistent');
 });
 
 QUnit.test('Token length test 2', function(assert) {
 	var test = '(+ (+ 1) 2 3)';
-	assert.equal(tokenise(test).length, 9, 'Number of tokens must be consistent');
+	assert.equal(t.tokenise(test).tokens.length, 9, 'Number of tokens must be consistent');
 });
 
 QUnit.test('String join test 1', function(assert) {
 	var test = '(print "hi!")';
-	assert.equal(tokenise(test).length, 4, 'Strings must be joined consistently');
+	assert.equal(t.tokenise(test).tokens.length, 4, 'Strings must be joined consistently');
 });
 
 QUnit.test('String join test 2', function(assert) {
 	var test = '(print "hi man!")';
-	assert.equal(tokenise(test).length, 4, 'Strings must be joined consistently');
+	assert.equal(t.tokenise(test).tokens.length, 4, 'Strings must be joined consistently');
 });
 
-QUnit.test('String join test 3', function(assert) {
-	var test = '(print "hi man, this is longer ( ( with some stuff!")';
-	assert.equal(tokenise(test).length, 4, 'Strings must be joined consistently');
+QUnit.test('Parens Balanced 1', function(assert) {
+	var test = '(+ 1 2)';
+	var tokens = t.tokenise(test).tokens;
+	assert.equal(p.parensBalanced(tokens), true, 'Parser must check for balanced parens');
+});
+
+QUnit.test('Parens Balanced 2', function(assert) {
+	var test = '(+ 1 ( 2)';
+	var tokens = t.tokenise(test).tokens;
+	assert.equal(p.parensBalanced(tokens), false, 'Parser must check for balanced parens');
+});
+
+QUnit.test('Parens Balanced 3', function(assert) {
+	var test = '(+ 1 (+ 4 5) (- 5 (- 6)) 2)';
+	var tokens = t.tokenise(test).tokens;
+	assert.equal(p.parensBalanced(tokens), true, 'Parser must check for balanced parens');
+});
+
+QUnit.test('Parser string check 1', function(assert) {
+	var test = '"My String"';
+	assert.equal(p.tokenIsString(test), true, 'Parser checks for broken strings');
+});
+
+QUnit.test('Parser string check 1', function(assert) {
+	var test = '"My string is broken';
+	assert.equal(p.tokenIsString(test), false, 'Parser checks for broken strings');
+});
+
+QUnit.test('Parser string check 1', function(assert) {
+	var test = '"My String "" is fine"';
+	assert.equal(p.tokenIsString(test), true, 'Parser checks for broken strings');
 });
 
