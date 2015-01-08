89c97148ab05c4dee9915560981aa5c3608800d3
maximinus <maximinus@gmail.com>
2015-01-08 15:53:50 +0800

    Much improved text rendering

--- js/interface.js
+++ js/interface.js
@@ -3,6 +3,7 @@
 // font characterisitics
 var FONT_WIDTH = 49;
 var FONT_HEIGHT = 102;
+var CHAR_WIDTH = 60;
 
 function loadFonts() {
 	game.load.image('monofur', 'data/fonts/monofur.png');
@@ -12,11 +13,19 @@ function createInterface() {
 	terminal.setup();
 };
 
+function ColourClass(r, g, b) {
+	// store colour values
+	this.r = r;
+	this.g = g;
+	this.b = b;
+};
+
 // class to handle text on a screen
 function Terminal() {
 	this.setup = function(width, height) {
 		this.strings = new Array();
-		this.colour = null;
+		// normal is white
+		this.colour = new ColourClass(255, 255, 255);
 		// precalculate sides
 		this.calculateArea()
 		this.calculateFontSize();
@@ -39,7 +48,7 @@ function Terminal() {
 	
 	this.calculateFontSize = function() {
 		// width and height are set by this point. 80 characters across
-		this.px_width = Math.floor(this.width / 80);
+		this.px_width = Math.floor(this.width / CHAR_WIDTH);
 		this.px_height = Math.floor(this.px_width * (FONT_HEIGHT / FONT_WIDTH));
 	};
 
@@ -58,29 +67,53 @@ function Terminal() {
 			image.copyRect('monofur', source_rect, xpos, 0);
 			xpos += FONT_WIDTH;
 		}
-		this.renderStringColour();
-		return(this.stringResize(image));
-		var fimage = game.add.bitmapData(this.px_width * chars.length, this.px_height);
-		var scale = fimage.width / image.width;
-		fimage.copy(image, 0, 0, image.width, image.height, 0, 0, fimage.width, fimage.height);
-		return(fimage);
+		//var fimage = game.add.bitmapData(this.px_width * chars.length, this.px_height);
+		
+		//fimage.copy(image, 0, 0, image.width, image.height, 0, 0, fimage.width, fimage.height);
+		image.update();
+		image.processPixelRGB(this.processPixel, this, 0, 0, image.width, image.height)
+		
+		return(image);
+	};
+	
+	this.test1 = function(string) {
+		// given a string, render to the correct size
+		// convert the string to an array of ASCII and subtract 32
+		var chars = new Array();
+		for(var i=0; i<string.length; i++) {
+			chars.push(string.charCodeAt(i) - 32); }
+		// make an empty image and blit font to it
+		var image = game.add.bitmapData(FONT_WIDTH * chars.length, FONT_HEIGHT);
+		// blit the letters
+		var xpos = 0;
+		for(var i in chars) {
+			var source_rect = new Phaser.Rectangle(chars[i] * FONT_WIDTH, 0, FONT_WIDTH, FONT_HEIGHT);			
+			image.copyRect('monofur', source_rect, xpos, 0);
+			xpos += FONT_WIDTH;
+		}
+		return(image);
+	};
+	
+	this.processPixel = function(colour) {
+		// colour is { r: number, g: number, b: number, a: number, color: number, rgba: string }
+		// we just preserve the alpha value
+		colour.r = this.colour.r;
+		colour.g = this.colour.g;
+		colour.b = this.colour.b;
+		return(colour);
 	};
 
-	this.renderStringColour = function() {
-		if(this.colour == null) {
-			return; }
-	};
-
-	this.setColour = function(new_colour) {
-		this.colour = new_colour;
-	};
-
-	this.print = function(x, y, text) {
+	this.print = function(x, y, text, colour) {
 		// get the image
+		console.log(colour);
 		var image = this.renderStringImage(text);
 		var xpos = (x * this.px_width) + this.xoffset;
 		var ypos = (y * this.px_height) + this.yoffset;
-		var sprite = game.add.sprite(xpos, ypos, image);
+		var scale = (text.length * this.px_width) / image.width;
+		var sprite = game.add.sprite(xpos, ypos, image)
+		// scale image
+		sprite.scale = new PIXI.Point(scale, scale);
+		this.strings.push(sprite);
 	};
 	
 	this.cls = function(x, y, text) {
@@ -91,9 +124,8 @@ function Terminal() {
 	};
 
 	this.create = function() {
-		this.print(0, 0, 'Scheme7 0.1 by Chris Handy');
-		this.print(0, 1, 'Test font functions');
-		this.print(2, 2, 'This line is offset by 2');
+		this.print(0, 0, 'Scheme7 v0.01');
+		this.print(0, 1, 'Code & Design Chris Handy 2014');
 	};
 };
 
