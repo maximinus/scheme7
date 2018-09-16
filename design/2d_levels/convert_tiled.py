c08062a6570b04f1c6c4e860a16010b8f61593d6
Chris Handy <maximinus@gmail.com>
2018-09-16 12:19:28 +0100

    Terminal is on it's way to working

---
+++ design/2d_levels/convert_tiled.py
@@ -0,0 +1,94 @@
+import sys
+import json
+import pygame
+
+# example of loading data from an svg file
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
+
+    @property
+    def as_tuple(self):
+        return (self.x, self.y)
+
+
+class Path:
+    def __init__(self, points, x, y):
+        # convert
+        self.points = []
+        for i in points:
+            xpos = i['x'] + x
+            ypos = i['y'] + y
+            self.points.append(Point(xpos, ypos))
+
+    @property
+    def width(self):
+        return max([x.x for x in self.points])
+
+    @property
+    def height(self):
+        return max([x.x for x in self.points])
+
+    @property
+    def closed(self):
+        if len(self.point) == 0:
+            return False
+        # loop of 1 elements is always closed
+        return self.points[0] == self.points[-1]
+
+    def __repr__(self):
+        point_string = ['[{0},{1}]'.format(x.x, x.y) for x in self.points]
+        return '->'.join(point_string)
+
+
+def die(message='Unknown'):
+    """Helper function to print error and quit"""
+    print('  Error: {0}'.format(message))
+    sys.exit(False)
+
+
+def loadPathsFromJson(filename):
+    json_text = open(filename).read()
+    json_data = json.loads(json_text)
+    for i in json_data['layers']:
+        if i['name'] == 'Polygons':
+            return i['objects']
+
+
+def convertAllPaths(path_data):
+    return [Path(x['polyline'], x['x'], x['y']) for x in path_data]
+
+
+def drawPaths(paths):
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    # now draw to the screen
+    screen.fill((0, 0, 0))
+    for i in paths:
+        # start at point 1, continue
+        start = i.points[0]
+        for j in i.points[1:]:
+            pygame.draw.line(screen, (255, 255, 255), start.as_tuple, j.as_tuple)
+            start = j
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            # listening for the the X button at the top
+            if event.type == pygame.QUIT:
+                return
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    paths = loadPathsFromJson('tiled.json')
+    paths = convertAllPaths(paths)
+    drawPaths(paths)
+    print('  * Done')
--- js/TerminalScene.js
+++ js/TerminalScene.js
@@ -1,6 +1,111 @@
+/*
+
+How does the terminal work?
+
+There is a flashing block at one point
+When buttons are pressed, a character is drawn and the console moved to the right
+If the characters move too far to the right, they move to the next line down
+When the console moves, it switches to the start of the 'ON' pattern
+Backspace moves the cursor left and deletes the character
+Left moves the cursor OVER the character and reverses the character when the cursor flashes on
+When return is pressed, the cursor moves down a line and to the left, and the screen scrolls up
+Pressing up/down cycles through previous commands
+If a button is held down, there is a pause and then the character is drawn again
+Repeated buttondown events are instant
+
+Functions we need:
+
+* Get the size of a character
+* Draw the cursor
+* Accept a keypress
+
+*/
+
+// only render these chars
+var CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=<>?,./:";\'{}[] '
+
+// prevent key bubbling
+const BACKSPACE = 8;
+const RETURN =13;
+const STOP_BUBBLING = [BACKSPACE. RETURN];
+
+
+function can_render(char) {
+    if(char.length > 1) {
+        return false;
+    }
+    // check if char is in the list
+    return(CHARS.indexOf(char) > -1);
+};
+
+class TerminalCharacter {
+    constructor(character, text_image) {
+        this.character = character;
+        this.image = text_image;
+    };
+
+    destroy() {
+        this.image.destroy()
+    };
+};
+
+
+class Cursor {
+    constructor(width, height, font, scene) {
+        this.position = new Phaser.Geom.Rectangle(16, 16, width, height);
+        this.font = font;
+        this.timer = null;
+        this.scene = scene;
+    };
+
+    add(text_function) {
+        this.cursor = text_function(this.position.x, this.position.y, '█', this.font);
+        this.startFlash();
+    };
+
+    flash() {
+        this.cursor.visible = !this.cursor.visible;
+    };
+
+    startFlash() {
+        console.log(this.scene.time.addEvent);
+        this.timer = this.scene.time.addEvent({delay: 800, callback: this.flash,
+                                               callbackScope: this, loop: true});
+    };
+
+    stopFlash() {
+        // stop the cursor animation
+        if(this.timer != null) {
+            this.timer.destroy();
+        };
+    };
+
+    update(deltax, deltay) {
+        this.stopFlash();
+        this.cursor.visible = true;
+        // move to new place
+        this.cursor.x += deltax;
+        this.cursor.y += deltay;
+        this.startFlash();
+    };
+
+    destroy() {
+        this.image.destroy()
+    };
+};
+
+
 class TerminalScene extends Phaser.Scene {
     constructor() {
         super({key: 'TerminalScene'});
+        this.current_line = [];
+        this.font = {fontFamily: 'scheme7-terminal',
+                     fontSize: '32px',
+                     color: '#C85746'};
+        this.char_width = 20;
+        this.char_height = 35;
+        this.xpos = 16;
+        this.ypos = 16;
     };
 
     preload() {
@@ -8,13 +113,45 @@ class TerminalScene extends Phaser.Scene {
     };
 
     create() {
-        var ttf = this.add.text(100, 100, '(format "Hello, World!")', 
-            {fontFamily: 'scheme7-terminal',
-             fontSize: '32px',
-             color: '#C85746'});
+        this.cursor = new Cursor(this.char_width, this.char_height, this.font, this);
+        this.cursor.add(this.add.text.bind(this.add));
+        // we need to tell Phaser what keys to not bubble
+        for(var keycode of STOP_BUBBLING) {
+            this.input.keyboard.addKey(keycode);
+        }
+        this.input.keyboard.on('keydown', this.keydown, this);
     };
 
     update() {
         // pass
+    };
+
+    keydown(event) {
+        console.log(event);
+        // we start by looking for special keys
+        if(event.keyCode === BACKSPACE) {
+            this.cursor.update(-this.char.width, 0);
+            // delete the last character if we have one
+            if(this.current_line.length > 0) {
+                this.current_line.pop().destroy();
+                this.xpos -= this.char_width;
+            }
+            return;
+        }
+
+        // if we can't render, don't
+        if(!can_render(event.key)) {
+            return;
+        }
+        this.cursor.update(this.char_width, 0);
+        var new_text = this.printCharacter(event.key);
+        this.current_line.push(new TerminalCharacter(event.key, new_text));
+    };
+
+    printCharacter(character) {
+        // print the character, move the cursor
+        var new_text = this.add.text(this.xpos, this.ypos, character, this.font);
+        this.xpos += this.char_width;
+        return new_text;
     };
 };
