3deb27db15013da78333e4e718f420ceef3c664b
maximinus <maximinus@gmail.com>
2021-01-24 21:34:51 +0000

    Added objectives text

--- Scheme7/Node2D.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready():
-	pass # Replace with function body.
-
-func updateDial(force):
-	# called with a vec2 of the force
-	pass
--- Scheme7/globals.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var last_movement = Vector2(0, 0)
-
-func _ready():
-	pass
