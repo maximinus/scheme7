08990a2e07136d6a843397434c7f2bf72c081198
maximinus <maximinus@gmail.com>
2021-01-25 21:08:48 +0000

    Added electric barrier animation

---
+++ Scheme7/scenes/LevelObjects/ElectricBarrier/ElectricBarrier.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+enum FACING { Vertical, Horizontal }
+export(FACING) var direction = FACING.Horizontal setget setFacing
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func setFacing(new_facing):
+	pass
